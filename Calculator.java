1fc50dad992e5d569a531f214357cd858dae63ef
Daniel Yang <danielyangkang@gmail.com>
2019-10-14 12:02:14 -0700

    asdf

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,145 @@
-import java.util.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-public class Calculator
-{
+import java.util.*;
+
+public class Calculator {
+    public void add(){
+        Main main = new Main();
+        main.getFrame();
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter numbers to add");
+        JTextField field = new JTextField(15);
+        field.addKeyListener(new KeyAdapter() //field accept numbers only
+            {
+                public void keyTyped(KeyEvent ke)
+                {
+                 char c = ke.getKeyChar();
+                    if(!Character.isDigit(c) && (c != '\b'))
+                    {
+                        ke.consume();
+                    }
+                }
+                public void keyReleased(KeyEvent e){}
+                public void keyPressed(KeyEvent e){}
+            });
+        JButton enter = new JButton("Enter");
+        JButton equals = new JButton("Equals");
+        JButton New = new JButton("New");
+        JTextArea area = new JTextArea();
+        area.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        area.setEditable(false);
+        
+        ArrayList<Integer> numList = new ArrayList<>();
+        enter.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String fieldnumber = field.getText();
+                field.setText("");
+                int num = Integer.parseInt(fieldnumber);
+                numList.add(num);
+                area.append(num + " " + "+" + " ");
+            }
+        });
+        
+        equals.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int sum = 0;
+                for (int i : numList){
+                    sum = sum + i;
+                }
+                String StrSum = Integer.toString(sum);
+                String result = area.getText().substring(0,area.getText().length() - 2);
+                area.setText(result);
+                area.append(" = " + StrSum);
+                field.setEditable(false);
+            }
+        });
+        
+        New.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                numList.clear();
+                area.setText("");
+                field.setEditable(true);
+                field.setText("");
+            }
+        });
+        panel.add(label);
+        panel.add(field);
+        panel.add(enter);
+        panel.add(equals);
+        panel.add(New);
+        main.getFrame().add(BorderLayout.SOUTH, panel);
+        main.getFrame().add(area);
+        main.getFrame().setVisible(true);
+    }
+    
+    public void subtract() {
+        Main main = new Main();
+        main.getFrame();
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter numbers to subtract");
+        JTextField field = new JTextField(15);
+        field.addKeyListener(new KeyAdapter() //field accept numbers only
+            {
+                public void keyTyped(KeyEvent ke)
+                {
+                 char c = ke.getKeyChar();
+                    if(!Character.isDigit(c) && (c != '\b'))
+                    {
+                        ke.consume();
+                    }
+                }
+                public void keyReleased(KeyEvent e){}
+                public void keyPressed(KeyEvent e){}
+            });
+        JButton enter = new JButton("Enter");
+        JButton equals = new JButton("Equals");
+        JButton New = new JButton("New");
+        JTextArea area = new JTextArea();
+        area.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        area.setEditable(false);
+        
+        ArrayList<Integer> numList = new ArrayList<>();
+        enter.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                String fieldnumber = field.getText();
+                field.setText("");
+                int num = Integer.parseInt(fieldnumber);
+                numList.add(num);
+                area.append(num + " " + "-" + " ");
+            }
+        });
+        
+        equals.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                int sum = 2 * numList.get(0);
+                for (int i : numList){
+                    sum = sum - i;
+                }
+                String StrSum = Integer.toString(sum);
+                String result = area.getText().substring(0,area.getText().length() - 2);
+                area.setText(result);
+                area.append(" = " + StrSum);
+                field.setEditable(false);
+            }
+        });
+        
+        New.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                numList.clear();
+                area.setText("");
+                field.setEditable(true);
+                field.setText("");
+            }
+        });
+        panel.add(label);
+        panel.add(field);
+        panel.add(enter);
+        panel.add(equals);
+        panel.add(New);
+        main.getFrame().add(BorderLayout.SOUTH, panel);
+        main.getFrame().add(area);
+        main.getFrame().setVisible(true);
+    }
     
 }
--- Main.java
+++ Main.java
@@ -3,15 +3,17 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class Main{
+public class Main extends JFrame{
     public Main(){
         frame();
     }
+    
     JFrame frame;
     public void frame(){
+        
         frame = new JFrame("Portfolio");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(600,400));
+        frame.setSize(new Dimension(700,400));
         frame.setLocationRelativeTo(null);
         
         JMenuItem monkeyJump, pigLatin, palindrome, shorthand;
@@ -22,10 +24,17 @@ public class Main{
             add = new JMenuItem("Add");
             add.addActionListener(new ActionListener(){
                 public void actionPerformed(ActionEvent e){
-                    add();
+                    Calculator calculator = new Calculator();
+                    calculator.add();
                 }
             });
             subtract = new JMenuItem("Subtract");
+            subtract.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    Calculator calculator = new Calculator();
+                    calculator.subtract();
+                }
+            });
             multiply = new JMenuItem("Multiply"); 
         pigLatin = new JMenuItem("PigLatinator");
         palindrome = new JMenuItem("Palindrome");
@@ -47,71 +56,8 @@ public class Main{
         frame.setVisible(true);
     }
     
-    public void add(){
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel("Enter numbers to add");
-        JTextField field = new JTextField(15);
-        field.addKeyListener(new KeyAdapter() //field accept numbers only
-            {
-                public void keyTyped(KeyEvent ke)
-                {
-                 char c = ke.getKeyChar();
-                    if(!Character.isDigit(c) && (c != '\b'))
-                    {
-                        ke.consume();
-                    }
-                }
-                public void keyReleased(KeyEvent e){}
-                public void keyPressed(KeyEvent e){}
-            });
-        JButton enter = new JButton("Enter");
-        JButton equals = new JButton("Equals");
-        JButton New = new JButton("New");
-        JTextArea area = new JTextArea();
-        area.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        area.setEditable(false);
-        
-        ArrayList<Integer> numList = new ArrayList<>();
-        enter.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                String fieldnumber = field.getText();
-                field.setText("");
-                int num = Integer.parseInt(fieldnumber);
-                numList.add(num);
-                area.append(num + " " + "+" + " ");
-            }
-        });
-        
-        equals.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                int sum = 0;
-                for (int i : numList){
-                    sum = sum + i;
-                }
-                String StrSum = Integer.toString(sum);
-                String result = area.getText().substring(0,area.getText().length() - 2);
-                area.setText(result);
-                area.append(" = " + StrSum);
-                field.setEditable(false);
-            }
-        });
-        
-        New.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                numList.clear();
-                area.setText("");
-                field.setEditable(true);
-                field.setText("");
-            }
-        });
-        panel.add(label);
-        panel.add(field);
-        panel.add(enter);
-        panel.add(equals);
-        panel.add(New);
-        frame.getContentPane().add(BorderLayout.SOUTH, panel);
-        frame.add(area);
-        frame.setVisible(true);
+    public Frame getFrame() {
+        return frame;
     }
     
     public static void main(String args[])
